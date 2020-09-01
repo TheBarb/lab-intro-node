07ab753b6e9b4627946795ed5c7ddbaab88993d4
mail@studio7berlin.de <mailstudio7berlin.de@mailstudio7berlindes-MacBook-Pro.local>
2020-09-01 15:17:03 +0200

    Tolga is a good coach

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    this.items.sort((a, b) => a - b)
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error('OutOfBounds')
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+    return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+    return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0
+    }
+    return this.items.reduce((acc, val) => {
+      return acc += val
+    })
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+    return this.sum() / this.items.length
+  }
 }
 
 module.exports = SortedList;
